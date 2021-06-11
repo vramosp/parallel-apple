a36c914e8ef0efd72e3d3d398db290df77814a8e
Stackbit <projects@stackbit.com>
2021-06-11 15:20:54 +0000

    initial commit by Stackbit

---
+++ stackbit-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+echo "stackbit-build.sh: start build"
+
+# fetch data from CMS through stackbit-pull
+npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/60c37f1e3c87150018483f66
+
+# build site
+hugo
+
+echo "stackbit-build.sh: finished build"
